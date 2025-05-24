65abf6a57722f4ab7b432a1ef9e0f05cc503a1fe
rammy427 <sebastiangonzalez004@gmail.com>
2025-05-24 15:54:15 +0000

    Updated insertion.

--- insert.sql
+++ insert.sql
@@ -1,7 +1,7 @@
-INSERT INTO books (book_id, title, published_year, isbn) VALUES
-(1, "Juan Salvador Gaviota", 1970, "8498721725"),
-(2, "El peligroso caso de Donald Trump", 2017, "1250179459"),
-(3, "La Iliada", 1489, NULL);
+INSERT INTO books (book_id, title, published_year, isbn, audio_path) VALUES
+(1, "Juan Salvador Gaviota", 1970, "8498721725", "lib/gaviota.mp3"),
+(2, "El peligroso caso de Donald Trump", 2017, "1250179459", "lib/trump.mp3"),
+(3, "La Iliada", 1489, NULL, "lib/iliada.mp3");
 
 INSERT INTO authors VALUES
 (1, "Richard", "Bach"),
