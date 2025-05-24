4fcc4022fb37f87a92b2860f46fe326de3ba961a
rammy427 <sebastiangonzalez004@gmail.com>
2025-05-24 21:29:17 +0000

    Editing books successfully.

--- insert.php
+++ insert.php
@@ -8,24 +8,32 @@ function isEditing(): bool
 
 function insert($connection, $title, $published_year, $isbn, $audio_path, $first_name, $last_name): void
 {
-    echo "I am inserting!";
     // Execute the book and author insertions.
-    // $book_query = "INSERT INTO books (title, published_year, isbn, audio_path) VALUES ('$title', $published_year, '$isbn', '$audio_path')";
-    // $author_query = "INSERT INTO authors (first_name, last_name) VALUES ('$first_name', '$last_name')";
-    // if (mysqli_query($connection, $book_query) && mysqli_query($connection, $author_query))
-    // {
-    //     // Insert the relation between the new book and author. Check IDs first.
-    //     $book_id = mysqli_fetch_array(mysqli_query($connection, "SELECT book_id FROM books WHERE title = '$title'"))[0];
-    //     $author_id = mysqli_fetch_array(mysqli_query($connection, "SELECT author_id FROM authors WHERE first_name = '$first_name'"))[0];
-    //     $relation_query = "INSERT INTO book_authors VALUES ($book_id, $author_id)";
-    //     if (mysqli_query($connection, $relation_query))
-    //         header("Location: options.php");
-    // }
+    $book_query = "INSERT INTO books (title, published_year, isbn, audio_path) VALUES ('$title', $published_year, '$isbn', '$audio_path')";
+    $author_query = "INSERT INTO authors (first_name, last_name) VALUES ('$first_name', '$last_name')";
+    if (mysqli_query($connection, $book_query) && mysqli_query($connection, $author_query))
+    {
+        // Insert the relation between the new book and author. Check IDs first.
+        $book_id = mysqli_fetch_array(mysqli_query($connection, "SELECT book_id FROM books WHERE title = '$title'"))[0];
+        $author_id = mysqli_fetch_array(mysqli_query($connection, "SELECT author_id FROM authors WHERE first_name = '$first_name' AND last_name = '$last_name'"))[0];
+        $relation_query = "INSERT INTO book_authors VALUES ($book_id, $author_id)";
+        if (mysqli_query($connection, $relation_query))
+            header("Location: options.php");
+    }
 }
 
 function update($connection, $book_id, $title, $published_year, $isbn, $audio_path, $first_name, $last_name): void
 {
-    echo "I am editing!";
+    // Execute the book update and author insertion.
+    $book_query = "UPDATE books SET title = '$title', published_year = $published_year, isbn = '$isbn', audio_path = '$audio_path' WHERE book_id = $book_id";
+    $author_query = "INSERT INTO authors (first_name, last_name) VALUES ('$first_name', '$last_name')";
+    if (mysqli_query($connection, $book_query) && mysqli_query($connection, $author_query))
+    {
+        $author_id = mysqli_fetch_array(mysqli_query($connection, "SELECT author_id FROM authors WHERE first_name = '$first_name' AND last_name = '$last_name'"))[0];
+        $relation_query = "UPDATE book_authors SET author_id = $author_id WHERE book_id = $book_id";
+        if (mysqli_query($connection, $relation_query))
+            header("Location: options.php");
+    }
 }
 
 // Get all submitted form values.
