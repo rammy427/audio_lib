27f544921ab4cc8b2c67cac4b63f226aa3b54fa5
rammy427 <sebastiangonzalez004@gmail.com>
2025-05-25 02:36:02 +0000

    Removed SQL query files.

--- delete.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE user_books;
-DROP TABLE book_authors;
-DROP TABLE books;
-DROP TABLE authors;
-DROP TABLE users;
--- insert.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users VALUES
-(1, "sebastian.ramirez4@upr.edu", "password", "Sebastián", "Ramírez");
